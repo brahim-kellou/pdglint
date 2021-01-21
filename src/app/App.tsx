01638e0c1d044d04f456fc8cd8ea506de7031078
brahim-kellou <fb_kellou@esi.dz>
2021-01-21 22:38:50 +0100

    refactor: structure the app

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Typing from '../components/Typing';
+
+function App() {
+  return (
+    <div className="App">
+      <Typing />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Typing.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const Typing = () => {
+  return (
+    <h1>Typing</h1>
+  );
+}
+
+export default Typing;
