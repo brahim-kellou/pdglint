4a6ecfdf8a06a5ac7e74b134937d0ffbbedc1ef1
brahim-kellou <fb_kellou@esi.dz>
2021-01-22 03:24:08 +0100

    feat(typing): update onKeyPress & onKeyUp

--- src/components/Typing.tsx
+++ src/components/Typing.tsx
@@ -1,9 +1,26 @@
 import { useEffect, useState } from 'react';
-import { PrimaryButton, Spinner, SpinnerSize } from '@fluentui/react';
+import faker from 'faker';
+import { getKeystrokeHand, getDate, getTimestamp } from '../utils';
+import { DefaultButton, Spinner, SpinnerSize, ProgressIndicator } from '@fluentui/react';
 import './Typing.css';
 
+const wordsList: string = new Array<string>(15)
+  .fill('')
+  .map(() => faker.random.word().toLowerCase())
+  .join(' ');
+
 const Typing = () => {
+  const [outgoingText, setOutgoingText] = useState<string>('');
+  const [currentChar, setCurrentChar] = useState<string>(wordsList.charAt(0));
+  const [incomingText, setIncomingText] = useState<string>(wordsList.substring(1));
+  const [progress, setProgress] = useState<number>(0);
   const [isTypingEnd, setIsTypingEnd] = useState<boolean>(false);
+  const [data, setData] = useState<any[]>([]);
+  const [dates, setDates] = useState<any[]>([]);
+
+  console.log(outgoingText)
+  console.log(currentChar)
+  console.log(incomingText)
 
   useEffect(() => {
     document.addEventListener('keypress', onKeyPress);
@@ -13,15 +30,62 @@ const Typing = () => {
       document.removeEventListener('keypress', onKeyPress);
       document.removeEventListener('keyup', onKeyUp);
     }
-  }, []);
+  });
 
-  const onKeyPress = () => {
+  useEffect(() => {
+    setProgress(
+      outgoingText.length / wordsList.length
+    );
+  }, [outgoingText])
 
+  const onKeyPress = ({ key }: any) => {
+    const hand = getKeystrokeHand(key)
+    if (hand) {
+      const date = new Date();
+      setDates(dates.concat([date]));
+      setData(data.concat([{
+        Date: getDate(date),
+        TS: getTimestamp(date),
+        Hand: hand,
+        HoldTime: 20,
+        Direction: data.length > 0
+          ? `${data[data.length - 1].Hand}${hand}`
+          : null,
+        LatencyTime: data.length > 0
+          ? date.getTime() - dates[dates.length - 1].getTime()
+          : null,
+        FlightTime: data.length > 0
+          ? date.getTime() - dates[dates.length - 1].getTime() - data[data.length - 1].HoldTime
+          : null,
+      }]));
+    }
   }
 
-  const onKeyUp = () => {
-
+  const onKeyUp = ({ key }: any) => {
+    if (key === currentChar) {
+      const date = new Date();
+      setData(() => {
+        if (data.length > 0) {
+          data[data.length - 1] = {
+            ...data[data.length - 1],
+            HoldTime: date.getTime() - dates[dates.length - 1].getTime(),
+          };
+        }
+        return data;
+      });
+      keystrokeLogger(key);
+    }
   }
+
+  const keystrokeLogger = (key: any) => {
+    if (key === currentChar) {
+      setOutgoingText(outgoingText + currentChar);
+
+      setCurrentChar(incomingText.charAt(0));
+
+      setIncomingText(incomingText.substring(1));
+    }
+  };
 
   const handleOnSubmit = () => {
     setIsTypingEnd(true);
@@ -29,25 +93,29 @@ const Typing = () => {
 
   return (
     <div className="typing-container">
-      <div>
+      <div className="text-container">
         <p>
-          <span>typed text - </span>
-          <span>current char - </span>
-          <span>upcoming text</span>
+          <span>{outgoingText}</span>
+          <span className="current-char">{currentChar}</span>
+          <span>{incomingText}</span>
         </p>
       </div>
-      <div>
-        <PrimaryButton
+      <div className="progress-container">
+        <h5 className="progress-text">Progress: {(progress * 100).toFixed(2)} %</h5>
+        <ProgressIndicator className="progress-indicator" percentComplete={progress} />
+      </div>
+      <div className="button-container">
+        <DefaultButton
           className="button"
           onClick={handleOnSubmit}
-          disabled={isTypingEnd}
+          disabled={isTypingEnd || (progress < 1)}
         >
           {isTypingEnd ? (
             <Spinner size={SpinnerSize.medium} />
           ) : (
               "Start analyzing"
             )}
-        </PrimaryButton>
+        </DefaultButton>
       </div>
     </div>
   );
---
+++ src/utils.ts
@@ -0,0 +1,24 @@
+import { leftHand, rightHand, spaceHand } from './data';
+
+export const getKeystrokeHand = (key: string) => {
+  if (leftHand.includes(key.toLocaleLowerCase())) {
+    return 'L';
+  } else if (rightHand.includes(key.toLocaleLowerCase())) {
+    return 'R';
+  } else if (spaceHand.includes(key.toLocaleLowerCase())) {
+    return 'S';
+  }
+}
+
+export const getDate = (date: any) => {
+  return (
+    `${(`0${date.getFullYear()}`).slice(-2)}${(`0${date.getMonth() + 1}`).slice(-2)}${(`0${date.getDate()}`).slice(-2)}`
+  );
+}
+
+export const getTimestamp = (date: any) => {
+  return (
+    `${(`0${date.getHours()}`).slice(-2)}:${(`0${date.getMinutes()}`).slice(-2)}:${(`0${date.getSeconds()}`).slice(-2)}.${JSON.stringify(date).substring(21, 24)}`
+  )
+}
+
